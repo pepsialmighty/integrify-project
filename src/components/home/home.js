a0cc156b499a2750919dd198cb45629d177b7969
pepsialmighty <hainguyenn257@gmail.com>
2020-12-11 16:17:06 +0200

    complete app 3.1.0

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Skeleton from '@material-ui/lab/Skeleton';
+import { gsap } from 'gsap';
 
 import './home.css';
 import MyCard from '../card/card';
+import { URL } from '../config/path';
 
 const Home = () => {
   const [userInfo, setUserInfo] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       const respone = await axios
-        .get('https://jsonplaceholder.typicode.com/users')
+        .get(`${URL}`)
         .then((res) => res.data)
         .catch((err) => console.log(err));
       setUserInfo(respone);
@@ -18,11 +21,35 @@ const Home = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    gsap.from('.home__card', {
+      opacity: 0,
+      y: 100,
+      stagger: 0.1,
+      duration: 1,
+    });
+  }, [userInfo]);
+
   return (
     <div className='home__container'>
+      <div className='home__title'>
+        <h1>HOME PAGE</h1>
+      </div>
       <div className='home__cards'>
-        {userInfo &&
-          userInfo.map((info) => <MyCard key={info.id} data={info} />)}
+        {userInfo
+          ? userInfo.map((info) => (
+              <div key={info.id} className='home__card'>
+                <MyCard data={info} />
+              </div>
+            ))
+          : [1, 2, 3, 4, 5, 6].map((i) => (
+              <div key={i}>
+                <Skeleton variant='text' width={200} height={50} />
+                <Skeleton variant='rect' width={250} height={80} />
+                <Skeleton variant='text' width={200} height={50} />
+                <Skeleton variant='rect' width={250} height={80} />
+              </div>
+            ))}
       </div>
       <div className='home__footer'>
         <p>© Copyright 2020 Nguyen Nguyen. All Rights Reserved.</p>
--- src/components/user-info/user-info.js
+++ src/components/user-info/user-info.js
@@ -3,9 +3,12 @@ import { useParams, useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Skeleton from '@material-ui/lab/Skeleton';
 import axios from 'axios';
+import { gsap } from 'gsap';
 
 import './user-info.css';
 import { useStateValue } from '../../StateProvider';
+
+import avatar from '../../assets/avat.jpg';
 
 const UserInfo = () => {
   let { id } = useParams();
@@ -14,21 +17,17 @@ const UserInfo = () => {
   const [{ userData }] = useStateValue();
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    setData(userData[0]);
-    console.log('hohoo');
-  }, [userData]);
-
-  console.log(data);
   const handleClick = () => {
     history.push('/');
   };
 
+  useEffect(() => {
+    setData(userData[0]);
+  }, [userData]);
+
   // If the application can not get the data, refetch the data
   useEffect(() => {
     if (!userData.length) {
-      // history.push('/');
-      // alert('Please try again!');
       const refetch = async () => {
         const response = await axios
           .get(`https://jsonplaceholder.typicode.com/users/${id}`)
@@ -36,7 +35,6 @@ const UserInfo = () => {
           .catch((err) => console.log(err));
 
         setData(response);
-        console.log('hoh');
       };
 
       refetch();
@@ -45,12 +43,29 @@ const UserInfo = () => {
     }
   }, []);
 
+  useEffect(() => {
+    gsap.from('.userInfo__wrapper', {
+      y: 100,
+      duration: 1,
+    });
+  }, []);
+
+  useEffect(() => {
+    let tl = gsap.timeline();
+
+    tl.from('.userInfo__wrapper', {
+      duration: 1,
+    });
+    tl.from('.userInfo__right', { x: 300, opacity: 0, duration: 1 });
+    tl.from('.userInfo__left', { x: -60, duration: 1 });
+  }, [data]);
+
   return (
     <div className='userInfo__container'>
+      <div className='userInfo__title'>
+        <h1>User Detail Page</h1>
+      </div>
       <div className='userInfo__wrapper'>
-        <div className='userInfo__title'>
-          <h1>User Detail Page</h1>
-        </div>
         {!data ? (
           <div>
             <Skeleton variant='text' width={150} />
@@ -62,61 +77,68 @@ const UserInfo = () => {
           </div>
         ) : (
           <div className='userInfo__content'>
-            <div className='userInfo__info'>
-              <h3>Name: {data.name}</h3>
+            <div className='userInfo__left'>
+              <div className='userInfo__avatar'>
+                <img src={avatar} alt='' />
+              </div>
             </div>
-            <div className='userInfo__info'>
-              <h3>Username: {data.username}</h3>
-            </div>
-            <div className='userInfo__info'>
-              <h3>
-                Email: <a href={`mailto:${data.email}`}>{data.email}</a>
-              </h3>
-            </div>
-            <div className='userInfo__info'>
-              <h3>
-                Phone: <a href={`tel:${data.phone}`}>{data.phone}</a>
-              </h3>
-            </div>
-            <div className='userInfo__info'>
-              <h3>
-                Website: <a href={data.website}>{data.website}</a>
-              </h3>
-            </div>
-            <div className='userInfo__info'>
-              <h3>
-                Company:
-                <ul>
-                  <li>Name: {data.company.name}</li>
-                  <li>Catch phase: {data.company.catchPhrase}</li>
-                  <li>BS: {data.company.bs}</li>
-                </ul>
-              </h3>
-            </div>
-            <div className='userInfo__info'>
-              <h3>
-                Address:
-                <ul>
-                  <li>Street: {data.address.street}</li>
-                  <li>Suite: {data.address.suite}</li>
-                  <li>City: {data.address.city}</li>
-                  <li>Zipcode: {data.address.zipcode}</li>
-                  <li>Geo Lat: {data.address.geo.lat}</li>
-                  <li>Geo LNG: {data.address.geo.lng}</li>
-                </ul>
-              </h3>
+            <div className='divider'></div>
+            <div className='userInfo__right'>
+              <div className='userInfo__right__content'>
+                <div className='userInfo__info'>
+                  <h3>Name: {data.name}</h3>
+                </div>
+                <div className='userInfo__info'>
+                  <h3>Username: {data.username}</h3>
+                </div>
+                <div className='userInfo__info'>
+                  <h3>
+                    Email: <a href={`mailto:${data.email}`}>{data.email}</a>
+                  </h3>
+                </div>
+                <div className='userInfo__info'>
+                  <h3>
+                    Phone: <a href={`tel:${data.phone}`}>{data.phone}</a>
+                  </h3>
+                </div>
+                <div className='userInfo__info'>
+                  <h3>
+                    Website: <a href={data.website}>{data.website}</a>
+                  </h3>
+                </div>
+                <div className='userInfo__info'>
+                  <h3>
+                    Company:
+                    <ul>
+                      <li>Name: {data.company.name}</li>
+                      <li>Catch phase: {data.company.catchPhrase}</li>
+                      <li>BS: {data.company.bs}</li>
+                    </ul>
+                  </h3>
+                </div>
+                <div className='userInfo__info'>
+                  <h3>
+                    Address:
+                    <ul>
+                      <li>Street: {data.address.street}</li>
+                      <li>Suite: {data.address.suite}</li>
+                      <li>City: {data.address.city}</li>
+                      <li>Zipcode: {data.address.zipcode}</li>
+                      <li>Geo Lat: {data.address.geo.lat}</li>
+                      <li>Geo LNG: {data.address.geo.lng}</li>
+                    </ul>
+                  </h3>
+                </div>
+              </div>
             </div>
           </div>
         )}
       </div>
-      <Button
-        variant='contained'
-        className='userInfo__btn'
-        color='primary'
-        onClick={handleClick}
-      >
-        Back
-      </Button>
+      <div className='userInfo__btn'>
+        <Button variant='contained' color='primary' onClick={handleClick}>
+          Back
+        </Button>
+      </div>
     </div>
   );
 };
